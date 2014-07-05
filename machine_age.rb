3751fa116486005b90d1e93ad209648234af2533
Pierre Sugar <pierre.sugar@siemens.com>
2014-07-05 19:45:10 +0200

    removing Git additions from source files

--- machine_age.rb
+++ machine_age.rb
@@ -13,10 +13,7 @@ def readme
   puts
   puts "1. Machine analysis"
   puts "-------------------"
-<<<<<<< HEAD
-=======
   puts "1.0 clean_ib_source (INFILE EUNA download)"
->>>>>>> 5b560c25ebf1a09efb65af53b81a7665c9027f5d
   puts "1.1 abc_analysis (INFILE: EUNA download)"
   puts "1.2 machine_age (INFILE: EUNA downlaod"
   puts "1.3 machine_count_top (INFILE: see note)"
@@ -40,22 +37,16 @@ def readme
   puts "    INFILE: DWH downlaod)"
 end
 
-<<<<<<< HEAD
 # Clean IB source
-=======
 # Clean IB source by removing leading 0 from IDs
->>>>>>> 5b560c25ebf1a09efb65af53b81a7665c9027f5d
 #
 # :call-seq:
 #   sycsvpro execute machine_age.rb clean_ib_source INFILE
 #
 # INFILE:: input csv-file sperated with colons (;) to operate on (EUNA downlaod)
 #
-<<<<<<< HEAD
 # Result is in the file 'INFILE_BASE_NAME-clean.csv'.
-=======
 # Result is in the file 'INFILE_BASE_NAME-clean.csv'
->>>>>>> 5b560c25ebf1a09efb65af53b81a7665c9027f5d
 def clean_ib_source
   infile, result, *others = params
   outfile = "#{File.basename(infile, '.*')}-clean.csv"
@@ -68,17 +59,13 @@ def clean_ib_source
 
   calculator = Sycsvpro::Calculator.new(infile:  infile,
                                         outfile: outfile,
-<<<<<<< HEAD
                                         header:  "*",
                                         rows:    "1-#{result.row_count}",
                                         cols:    cols).execute
- 
-=======
                                         header:  '*',
                                         rows:    "1-#{result.row_count}",
                                         cols:    cols).execute
 
->>>>>>> 5b560c25ebf1a09efb65af53b81a7665c9027f5d
   puts; puts "You can find the result in '#{outfile}'"
 end
 
@@ -100,11 +87,8 @@ def abc_analysis
 
   aggregator = Sycsvpro::Aggregator.new(infile:  infile, 
                                         outfile: "aggregate.csv", 
-<<<<<<< HEAD
                                         cols:    "46,45", 
-=======
                                         cols:    "45", 
->>>>>>> 5b560c25ebf1a09efb65af53b81a7665c9027f5d
                                         sum:     "Total:1,Machines")
 
   aggregator.execute
@@ -162,11 +146,8 @@ def machine_age
   infile, result, *others = params
   ages_filename = "machine-ages-#{others[0] || File.basename(infile, '.*')}.csv"
 
-<<<<<<< HEAD
   puts; print "Extracting date columns for #{infile}..."
-=======
   puts; print "Extracting date columns from #{infile}..."
->>>>>>> 5b560c25ebf1a09efb65af53b81a7665c9027f5d
 
   Sycsvpro::Extractor.new(infile: infile,
                           outfile: "extract.csv",
@@ -219,7 +200,6 @@ def machine_age
 
 end
 
-<<<<<<< HEAD
 =======
 # List all customers with machine count per year. Sum
 # up the machine count.
@@ -264,7 +244,6 @@ def machine_count_per_year
  
 end
 
->>>>>>> 5b560c25ebf1a09efb65af53b81a7665c9027f5d
 # Extracts the top customers based on machine count and age
 #
 # :call-seq:
@@ -360,13 +339,10 @@ def insert_customer_data
   Sycsvpro::Join.new(infile:        infile,
                      outfile:       outfile,
                      source:        source,
-<<<<<<< HEAD
                      cols:          "0,3;0,3",
                      joins:         "4=19;4=20",
-=======
                      cols:          "1,2;1,2",
                      joins:         "0=19;0=20",
->>>>>>> 5b560c25ebf1a09efb65af53b81a7665c9027f5d
                      pos:           "20,21;23;24",
                      insert_header: "OI_EK_NAME,OI_EK_LAND;OI_AG_NAME,OI_AG_LAND").execute
 
@@ -495,7 +471,7 @@ end
 #          #insert_customer_data)
 # COUNTRY_NAME:: country-identifier for the resulting file
 # REGION:: region-identifier for the resulting file
-##
+#
 # The result will be in 
 # 'COUNTRY_NAME-REGION-customer-revenues-per-year-and-type.csv' if no
 # COUNTRY_NAME or REGION is given the result is in
